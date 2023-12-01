ff7298d4f43fadfb6614c2afc7856f765a513e25
Loreto1306 <144122265+Loreto1306@users.noreply.github.com>
2023-12-01 17:04:04 -0300

    Agradecemos o Contato.js

--- script/script.js
+++ script/script.js
@@ -3,3 +3,15 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', ()=> {
     document.body.classList.toggle('dark')
 })
+
+let nome = document.getElementById("nome");
+let email = document.getElementById("email");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e)=>{
+    console.log(nome.value);
+    console.log(email.value)
+    e.preventDefault()
+    alert("Obrigado pelo Contato")
+    
+})
